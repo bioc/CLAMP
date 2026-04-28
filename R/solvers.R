2b9089c9e0d292b0dc160ca5b0adeaeed2bd028f
Alexander McKim <alexander.mckim@cuanschutz.edu>
2026-04-28 17:07:03 -0600

    declaring some column names

--- R/solvers.R
+++ R/solvers.R
@@ -2151,8 +2151,8 @@ CLAMPplotU <- function(clampRes, auc.cutoff = 0.6, fdr.cutoff = 0.05,
   U <- as.matrix(clampRes$U)
 
   pval.cutoff <- if (!is.null(clampRes$summary) &&
-                     any(clampRes$summary[, 5] < fdr.cutoff)) {
-    max(clampRes$summary[clampRes$summary[, 5] < fdr.cutoff, 4])
+                     any(clampRes$summary$FDR < fdr.cutoff)) {
+    max(clampRes$summary$p_value[clampRes$summary$FDR < fdr.cutoff])
   } else {
     Inf
   }
